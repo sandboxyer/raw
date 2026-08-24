effec9a10701517ede79d2b9ef376b884c5591ea
Daniel Massud <massudstudio@gmail.com>
2026-08-24 01:41:46 -0400

    spcmt

--- Raw.sh
+++ Raw.sh
@@ -2716,6 +2716,7 @@ display_compact_tool_list() {
         fi
     done <<< "$group_tools_map"
 
+    echo ""
     echo -e "\033[0;90mUse --tools for detailed view\033[0m"
     echo -e "\033[1;33mUsage: --<tool> [args]\033[0m"
 }
